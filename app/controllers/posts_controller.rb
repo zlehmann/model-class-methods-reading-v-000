37ed2e0e9376dce4e0adc53373bba4d7d8315ee3
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 22:38:13 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def index
     @authors = Author.all
     if !params[:author].blank?
-      @posts = Post.where(author: params[:author])
+      @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
       if params[:date] == "Today"
         @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,13 @@ class Post < ActiveRecord::Base
     where(author: author_id)
   end
 
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
   private
 
   def is_title_case
