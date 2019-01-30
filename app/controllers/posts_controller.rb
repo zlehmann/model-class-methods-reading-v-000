7a6ccc40108caa185a9427f750717baef167d85b
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 22:36:30 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,19 @@
 class PostsController < ApplicationController
-  helper_method :params
+
   def index
-    @posts = Post.all
+    @authors = Author.all
+    if !params[:author].blank?
+      @posts = Post.where(author: params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+      else
+        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+      end
+    else
+      # if no filters are applied, show all posts
+      @posts = Post.all
+    end
   end
 
   def show
